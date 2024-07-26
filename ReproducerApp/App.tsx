e3ed1f45da0b37049e8fdc91044e7681892fee3b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-07-26 11:09:55 -0700

    Remove unused styles

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, useColorScheme} from 'react-native';
+import {Button, SafeAreaView, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -31,24 +31,5 @@ function App(): React.JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
